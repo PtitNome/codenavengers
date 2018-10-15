a2c4362b433ca4800b7bff5432ace46de8ba859c
Olivier Milette <miletteolivier@gmail.com>
2018-10-14 23:39:21 -0400

    Clérical: contractions du this.props.navigation.state.params

--- Components/AvengerInfo.js
+++ Components/AvengerInfo.js
@@ -10,26 +10,26 @@ import { getAvengerImage } from '../Data/MarvelAPI'
 class AvengerInfo extends React.Component {
 
   render() {
-    /* On récupère l'objet avenger passé par les props.navigation.state.params
+    /* On récupère les objets avenger et thumbnail passés par les props.navigation.state.params
       en provenance du Navigateur */
+    const { avenger } = this.props.navigation.state.params
+    const { thumbnail } = this.props.navigation.state.params.avenger
     return (
       <View style={styles.main_container}>
         <View style={styles.view_container}>
           <Image
             style={styles.avg_img}
             source={
-              getAvengerImage(this.props.navigation.state.params.avenger.thumbnail.path,
-                this.props.navigation.state.params.avenger.thumbnail.extension).includes('image_not_available')?
+              getAvengerImage(thumbnail.path, thumbnail.extension).includes('image_not_available') ?
               require('../Images/image_not_available.png')
-              : {uri: getAvengerImage(this.props.navigation.state.params.avenger.thumbnail.path,
-                this.props.navigation.state.params.avenger.thumbnail.extension)} }
+              : {uri: getAvengerImage(thumbnail.path, thumbnail.extension)} }
           />
         </View>
         <ScrollView style={styles.scrollview_container}>
-          <Text style={styles.text_name}>Name : {this.props.navigation.state.params.avenger.name}</Text>
+          <Text style={styles.text_name}>Name : { avenger.name }</Text>
           <Text style={styles.default_text}>Real identity : Lorem Ipsum</Text>
           <Text style={styles.default_text}>Age : 38</Text>
-          <Text style={styles.description_text}>Description : {this.props.navigation.state.params.avenger.description}</Text>
+          <Text style={styles.description_text}>Description : { avenger.description }</Text>
           <Text style={styles.description_text}>Super power : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mollis eget quam sed ultricies. Vestibulum nec augue nunc. Proin eget aliquet tortor. Nunc at lectus eu elit sollicitudin feugiat eu non mi. Vivamus molestie nulla quis sapien malesuada, ut aliquet ante interdum. Phasellus id commodo metus, in finibus elit. Donec fringilla turpis eget ornare commodo. Donec malesuada tortor libero, ultrices pharetra velit euismod ac.       Vestibulum scelerisque tincidunt risus non consectetur. Cras lobortis ut dui auctor porttitor. Maecenas vitae sem nec velit porttitor pulvinar pretium nec risus. Proin eu erat tellus. Praesent varius neque vel aliquam egestas. In hac habitasse platea dictumst. Vestibulum elementum leo ac sem aliquet accumsan.
           </Text>
         </ScrollView>
